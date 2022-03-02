49ca3a45819634a8453c410827e8935a1baa515f
srmono <bvsrao91@gmail.com>
2022-03-02 06:33:04 +0530

    APOC data sources updated for practice

--- load_csv_exercise/data/export_csv.sql
+++ load_csv_exercise/data/export_csv.sql
@@ -5,4 +5,6 @@ COPY (SELECT * FROM employees) TO '/tmp/employees.csv' WITH CSV header;
 COPY (SELECT * FROM categories) TO '/tmp/categories.csv' WITH CSV header;
 
 COPY (SELECT * FROM orders
-      LEFT OUTER JOIN order_details ON order_details.OrderID = orders.OrderID) TO '/tmp/orders.csv' WITH CSV header;
+      LEFT OUTER JOIN order_details 
+      ON order_details.OrderID = orders.OrderID) 
+      TO '/tmp/orders.csv' WITH CSV header;
